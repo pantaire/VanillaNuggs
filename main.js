8e9230fef4d0b31f5568f0f9b62fd3c77d23099f
Kadda <kadda@taire.de>
2020-05-14 22:47:26 +0200

    this is one of the hills i will die on

--- main.js
+++ main.js
@@ -1,34 +1,126 @@
-let orders = [
+// VARIABLES ----------------------------------------------
+
+var exact = {
+    twens: 0,
+    nines: 0,
+    sixes: 0,
+    nuggcount: 0,
+    sauces: 0,
+    price: 0,
+};
+
+var easy = {
+    twens: 0,
+    nines: 0,
+    sixes: 0,
+    nuggcount: 0,
+    sauces: 0,
+    price: 0,
+};
+
+var cheap = {
+    twens: 0,
+    nines: 0,
+    sixes: 0,
+    nuggcount: 0,
+    sauces: 0,
+    price: 0,
+};
+
+var saucy = {
+    twens: 0,
+    nines: 0,
+    sixes: 0,
+    nuggcount: 0,
+    sauces: 0,
+    price: 0,
+};
+
+//can be modified through gui in later iterations
+var prices = {
+    price20: 7.99,
+    price9: 3.99,
+    price6: 2.99
+};
+
+//can be modified through gui in later iterations
+var dips = {
+    dip6: 1,
+    dip9: 2,
+    dip20: 3
+};
+
+var orders = [
     {
-        name: "Genaueste Anzahl",
-        abbr: "precise",
-        twens: "1",
-        nines: "3",
-        sixes: "4"
+        exact
     },
     {
-        name: "Einfache Bestellung",
-        abbr: "easy",
-        twens: "1",
-        nines: "3",
-        sixes: "4"
+        easy
     },
     {
-        name: "Bester Preis",
-        abbr: "cheap",
-        twens: "1",
-        nines: "3",
-        sixes: "4"
+        cheap
     },
     {
-        name: "Meiste Dips",
-        abbr: "saucy",
-        twens: "1",
-        nines: "3",
-        sixes: "4"
+        saucy
     }
 ]
 
+//CALCULATING --------------------------------------------------
+
+//just for testing 
+function math() {
+    var math = getInput() * 2;
+    document.getElementById("math").innerHTML = math;
+}
+
+var i;
+function calcExact(i) {
+
+var i = getInput();
+
+    //initialisierung mit s/n/t=0 nötig?
+    if (input <= 6) {
+        this.sixes = 1;
+        this.nines = 0;
+        this.twens = 0;
+    }
+    else if (input > 6 && input <= 9) {
+        this.sixes = 0;
+        this.nines = 1;
+        this.twens = 0;
+    }
+    else if (input > 9 && input <= 12) {
+        this.sixes = 2;
+        this.nines = 0;
+        this.twens = 0;
+    }
+    else if (input > 12 && input <= 20) {
+        this.sixes = 0;
+        this.nines = 0;
+        this.twens = 1;
+    }
+    else {
+        if (input % 6 == 0 || input % 6 >= 4 ) {
+            this.sixes = input / 6;
+        }
+        else if (input % 9 == 0 || input % 9 >= 6) {
+            this.nines = input / 9;
+        }
+        else if (input % 20 == 0 || input % 20 >= 14) {
+            this.twens = input / 20;
+        }
+    }
+    
+    this.nuggcount = this.sixes * 6 + this.nines * 9 + this.twens * 20;
+    this.dipcount = this.sixes * dip6 + this.nines * dip9 + this.twens * dip20;
+    this.price = this.sixes * price6 + this.nines * price9 + this.twens * price20;	
+return this;
+}
+
+
+//DISPLAYING --------------------------------------------------
+
+//onclick() action to process input 
 function toggleShowContent() {
   var content = document.getElementById("showContent");
   if (content.style.display === "none") {
@@ -38,20 +130,13 @@ function toggleShowContent() {
   }
   displayInput();
   showOrders();
+  math();
 }
 
-function toggleShowDisclaimer() {
-    var disclaimer = document.getElementById("disclaimer");
-    if (disclaimer.style.display === "none") {
-      disclaimer.style.display = "block";
-    } else {
-      disclaimer.style.display = "none";
-    }
-}
-
-function math() {
-    var math = getInput() * 2;
-    document.getElementById("math").innerHTML = math;
+//reads input -> can be integrated into ToggleShowContent()
+function getInput() {
+    var i = document.getElementById("input").value;
+    return i;
 }
 
 function getAllOrders() {
@@ -59,19 +144,25 @@ function getAllOrders() {
     showOrders();
 }
 
+//subfunction to show orders -> can be integrated into ToggleShowContent()
 function showOrders() {
     var orderList = document.getElementById("orderList");
     orderList.style.display = "block";
 }
 
+//reads input and displays it
 function displayInput() {
     var input;
     input = getInput();
     document.getElementById("inputHere").innerHTML = input;
 }
 
-
-function getInput() {
-    var i = document.getElementById("input").value;
-    return i;
+//shows disclaimer after clicking corr. button; can be modified for showing graphs later
+function toggleShowDisclaimer() {
+    var disclaimer = document.getElementById("disclaimer");
+    if (disclaimer.style.display === "none") {
+      disclaimer.style.display = "block";
+    } else {
+      disclaimer.style.display = "none";
+    }
 }
